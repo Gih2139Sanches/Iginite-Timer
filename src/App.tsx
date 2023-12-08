fb40277049b256627afc66ffbdba0accf158953c
Gih2139Sanches <giovanaclemente2139@gmail.com>
2023-12-08 14:33:31 -0300

    Configurações do Eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { ThemeProvider } from "styled-components"
-import { Button } from "./components/Button"
-import { defaultTheme } from "./styles/themes/default"
-import { GlobalStyle } from "./styles/global"
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Vite + React</h1>
-      <Button variant="primary"/>
+      <Button variant="primary" />
       <GlobalStyle />
     </ThemeProvider>
   )
