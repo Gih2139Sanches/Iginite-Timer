838a92d0dbf9458f45563e93d5266a760c9f6989
Gih2139Sanches <giovanaclemente2139@gmail.com>
2023-12-08 15:27:41 -0300

    Criação de Rotas

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from "./Button.styles.ts";
-
-interface ButtonProps{
-    variant?: ButtonVariant;
-}
-
-export function Button({variant: 'primary'}: ButtomProps){
-    return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
