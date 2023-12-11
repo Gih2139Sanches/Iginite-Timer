f51ead48ab2a8393f964de3ad54c3649a83eff4d
Gih2139Sanches <giovanaclemente2139@gmail.com>
2023-12-11 16:50:54 -0300

    Component Status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,56 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>Duração</td>
+              <td>Início</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>Duração</td>
+              <td>Início</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>Duração</td>
+              <td>Início</td>
+              <td>
+                <Status statusColor="yellow">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>Duração</td>
+              <td>Início</td>
+              <td>
+                <Status statusColor="red">Concluido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
